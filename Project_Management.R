bfe2178fc998bce6ce655345f2263e018f503d41
SamuelKalinowski <spkalinowski@ucdavis.edu>
2024-10-03 15:00:30 -0700

    Project_Management

---
+++ Project_Management.R
@@ -0,0 +1,7 @@
+getwd()
+"C:/Users/sam_k/OneDrive/Documents/UC Davis EPM/Fall Quarter 2024/ENV 224/R-Projects/R_DAVIS_in_class_project_SamuelKalinowskiNEW"
+
+d <- read.csv("./data/tail_length.csv")
+
+dir.create("./lectures")
+dir.create("./scripts")
