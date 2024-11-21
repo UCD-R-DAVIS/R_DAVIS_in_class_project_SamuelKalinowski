465d58c0c934f88d2abc850f10efbbb3603a3331
SamuelKalinowski <spkalinowski@ucdavis.edu>
2024-11-21 13:40:53 -0800

    Homework 8 with class notes added

--- scripts/Homework_8.R
+++ scripts/Homework_8.R
@@ -7,6 +7,7 @@ view(mloa)
 
 library(tidyverse)
 library(lubridate)
+library(ggplot2)
 
 view(mloa)
 
@@ -20,8 +21,14 @@ mloa2 <- mloa %>% filter(
     temp_C_2m != -999.9 &
     rel_humid != -99)
 
+    ##the way to remove missing values. Could also do:
+        ## mloa %>% 
+              #filter(windspeed > 0 & temp_C_m > 0 & rel_humid > 0)
+
 #Generate a column called “datetime” using the year, month, day, hour24, and min columns-----
 mloa2$datetime <- paste(mloa2$year, "-", mloa2$month,"-", mloa2$day, ", ", mloa2$hour24, ":", mloa2$min, sep = "")
+    #OR mloa2 %>% mutate(datetime, ymd_hm(paste(year,month,day,sep = '-'), paste(hour24, min, sep = ':')))
+
 
 view(mloa2)
 mloa2 %>% select(datetime, month, day, year, hour24, min) %>% head()
@@ -29,6 +36,7 @@ mloa2 %>% select(datetime, month, day, year, hour24, min) %>% tail()
 
 mloa2$datetime <- ymd_hm(mloa2$datetime, 
                         tz = "UTC")
+#
 
 tail(mloa2)
 #I think this worked! I used the video tutorial for help with this part!
@@ -49,6 +57,9 @@ mloa2 %>%
   group_by(month,hour) %>% 
   summarise(meanhourlytemp=mean(temp_C_2m))
 
+    #OR COULD HAVE DONE IT THIS WAY:
+      # mloa2 %>% mutate(hour_hst = hour(datetime_hst)) %>% group_by(hour24,month) %>%         summarise(meanhourlytemp = mean(temp_C_m))
+
 #Make a ggplot scatterplot of the mean monthly temperature, with points colored by local hour------
 mloa2 %>%
   mutate(month=month(datetime,label=TRUE)) %>% 
@@ -67,6 +78,6 @@ mloa2 %>%
 
 #YAY!
   
-
+#OR ggplot(data = mloa2, aes(x=month, y=meanhourlytemp, color = hour))+geom_point()+scale_color_distiller(palette = 'Oranges)
 
 
